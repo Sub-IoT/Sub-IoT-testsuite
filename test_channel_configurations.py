3e1ce24e0f1bb560b05ca0570f5402466c57889b
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2017-08-31 07:59:45 +0200

    check channel_header of received cmd is as expected

--- test_channel_configurations.py
+++ test_channel_configurations.py
@@ -16,29 +16,30 @@ from d7a.types.ct import CT
 from modem.modem import Modem
 
 
-def change_access_profile(modem, channel_band, channel_class, channel_coding=ChannelCoding.PN9, channel_index=0):
+def change_access_profile(modem, channel_header, channel_index=0):
   access_profile = AccessProfile(
-    channel_header=ChannelHeader(channel_band=ChannelBand.BAND_868,
-                                 channel_coding=ChannelCoding.PN9,
-                                 channel_class=ChannelClass.NORMAL_RATE),
+    channel_header=channel_header,
     sub_profiles=[SubProfile(subband_bitmap=0x01, scan_automation_period=CT(exp=0, mant=0)), SubProfile(), SubProfile(),
                   SubProfile()],
     sub_bands=[SubBand(
-      channel_index_start=0,
-      channel_index_end=0,
+      channel_index_start=channel_index,
+      channel_index_end=channel_index,
       eirp=10,
       cca=86  # TODO
     )]
   )
 
-  modem.execute_command(Command.create_with_write_file_action_system_file(file=AccessProfileFile(access_profile=access_profile, access_specifier=0)))
+  resp = modem.execute_command(Command.create_with_write_file_action_system_file(file=AccessProfileFile(access_profile=access_profile, access_specifier=0)))
+  assert resp, "Setting Access Profile failed!"
 
 
-def test_868N0(test_device, dut):
+def test_868_N_000(test_device, dut):
     dut.clear_unsolicited_responses_received() # TODO use pytest mechanism to do this for every test
-
-    change_access_profile(dut, ChannelBand.BAND_868, ChannelClass.NORMAL_RATE)
-    change_access_profile(test_device, ChannelBand.BAND_868, ChannelClass.NORMAL_RATE)
+    channel_header = ChannelHeader(channel_band=ChannelBand.BAND_868,
+                                   channel_coding=ChannelCoding.PN9,
+                                   channel_class=ChannelClass.NORMAL_RATE)
+    change_access_profile(dut, channel_header)
+    change_access_profile(test_device, channel_header)
 
     interface_configuration = Configuration(
         qos=QoS(resp_mod=ResponseMode.RESP_MODE_NO),
@@ -56,18 +57,25 @@ def test_868N0(test_device, dut):
     )
 
     resp = test_device.execute_command(command, timeout_seconds=10)
+    assert resp, "No response from test device"
 
     while len(dut.get_unsolicited_responses_received()) == 0: # endless loop, ended by pytest-timeout if needed
       pass
 
     assert len(dut.get_unsolicited_responses_received()) == 1, "DUT should have received 1 unsolicited response from test device"
 
+    assert(dut.get_unsolicited_responses_received()[0].get_d7asp_interface_status().channel_header == channel_header,
+      "Received using unexpected channel header")
 
-def test_868H0(test_device, dut):
+
+def test_868_H_000(test_device, dut):
   dut.clear_unsolicited_responses_received()  # TODO use pytest mechanism to do this for every test
+  channel_header = ChannelHeader(channel_band=ChannelBand.BAND_868,
+                                 channel_coding=ChannelCoding.PN9,
+                                 channel_class=ChannelClass.HI_RATE)
 
-  change_access_profile(dut, ChannelBand.BAND_868, ChannelClass.HI_RATE)
-  change_access_profile(test_device, ChannelBand.BAND_868, ChannelClass.HI_RATE)
+  change_access_profile(dut, channel_header)
+  change_access_profile(test_device, channel_header)
 
   interface_configuration = Configuration(
     qos=QoS(resp_mod=ResponseMode.RESP_MODE_NO),
@@ -85,9 +93,50 @@ def test_868H0(test_device, dut):
   )
 
   resp = test_device.execute_command(command, timeout_seconds=10)
+  assert resp, "No response from test device"
 
   while len(dut.get_unsolicited_responses_received()) == 0:  # endless loop, ended by pytest-timeout if needed
     pass
 
   assert len(
     dut.get_unsolicited_responses_received()) == 1, "DUT should have received 1 unsolicited response from test device"
+
+  assert (dut.get_unsolicited_responses_received()[0].get_d7asp_interface_status().channel_header == channel_header,
+          "Received using unexpected channel header")
+
+
+def test_868_L_000(test_device, dut):
+  dut.clear_unsolicited_responses_received()  # TODO use pytest mechanism to do this for every test
+  channel_header = ChannelHeader(channel_band=ChannelBand.BAND_868,
+                                 channel_coding=ChannelCoding.PN9,
+                                 channel_class=ChannelClass.LO_RATE)
+
+  change_access_profile(dut, channel_header)
+  change_access_profile(test_device, channel_header)
+
+  interface_configuration = Configuration(
+    qos=QoS(resp_mod=ResponseMode.RESP_MODE_NO),
+    addressee=Addressee(
+      access_class=0x01,
+      id_type=IdType.NOID,
+    )
+  )
+
+  command = Command.create_with_return_file_data_action(
+    file_id=0x40,
+    data=range(10),
+    interface_type=InterfaceType.D7ASP,
+    interface_configuration=interface_configuration
+  )
+
+  resp = test_device.execute_command(command, timeout_seconds=10)
+  assert resp, "No response from test device"
+
+  while len(dut.get_unsolicited_responses_received()) == 0:  # endless loop, ended by pytest-timeout if needed
+    pass
+
+  assert len(
+    dut.get_unsolicited_responses_received()) == 1, "DUT should have received 1 unsolicited response from test device"
+
+  assert (dut.get_unsolicited_responses_received()[0].get_d7asp_interface_status().channel_header == channel_header,
+          "Received using unexpected channel header")
