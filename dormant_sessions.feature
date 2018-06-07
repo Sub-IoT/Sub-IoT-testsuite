62d55c4cbfe7ff3e5e9d7dc9dd691ebb2d393b2b
Glenn Ergeerts <glenn.ergeerts@uantwerpen.be>
2018-06-07 15:28:23 +0200

    added more dormant sessions tests

--- dormant_sessions.feature
+++ dormant_sessions.feature
@@ -1,7 +1,7 @@
 Feature: Dormant session
 
 @wip
-Scenario: Registering a dormant session for a requester which does unicast requests
+Scenario: Dormant session succeeds for a requester which does unicast requests
     Given a requester, which does not scan
     And a responder, continuously listening for foreground packets
     And a dormant session registered at the responder for the UID of the requester
@@ -11,3 +11,29 @@ Scenario: Registering a dormant session for a requester which does unicast reque
     And the requester should receive the dormant session
     And the responders's dormant session should complete successfully
 
+@wip
+Scenario: Dormant session fails for a requester which does broadcast requests
+    Given a requester, which does not scan
+    And a responder, continuously listening for foreground packets
+    And a dormant session registered at the responder for the UID of the requester
+    When the requester starts a broadcast session to the responder
+    Then the requester's session should complete successfully
+    And the responder should receive an unsolicited response
+    And the requester should not receive the dormant session
+    # TODO And the responders's dormant session should not complete successfully
+
+@wip
+Scenario: Dormant session times out as expected and fails when no response
+    Given a requester, which does not scan
+    And a responder, continuously listening for foreground packets
+    And a dormant session registered at the responder for the UID of the requester
+    When waiting for the dormant session to time out
+    Then the responders's dormant session should not complete successfully
+
+@wip
+Scenario: Dormant session times out as expected and succeeds on response
+    Given a requester, which scans continuously
+    And a responder, continuously listening for foreground packets
+    And a dormant session registered at the responder for the UID of the requester
+    When waiting for the dormant session to time out
+    Then the responders's dormant session should complete successfully
--- test_dormant_sessions.py
+++ test_dormant_sessions.py
@@ -13,10 +13,21 @@ from d7a.types.ct import CT
 
 
 
-@scenario('dormant_sessions.feature', 'Registering a dormant session for a requester which does unicast requests')
+@scenario('dormant_sessions.feature', 'Dormant session succeeds for a requester which does unicast requests')
 def test_dormant_unicast():
   pass
 
+@scenario('dormant_sessions.feature', 'Dormant session fails for a requester which does broadcast requests')
+def test_dormant_broadcast():
+  pass
+
+@scenario('dormant_sessions.feature', 'Dormant session times out as expected and fails when no response')
+def test_dormant_timeout_with_response():
+  pass
+
+@scenario('dormant_sessions.feature', 'Dormant session times out as expected and succeeds on response')
+def test_dormant_timeout_without_response():
+  pass
 
 @given("a requester, which does not scan")
 def requester(test_device, default_channel_header, default_channel_index):
@@ -28,8 +39,22 @@ def requester(test_device, default_channel_header, default_channel_index):
                         specifier=1)
   set_active_access_class(test_device, 0x01)
   sleep(0.2)  # give some time to switch AP
+  test_device.clear_unsolicited_responses_received()
   return test_device
 
+
+@given("a requester, which scans continuously")
+def requester_scanning(test_device, default_channel_header, default_channel_index):
+  change_access_profile(test_device,
+                        create_access_profile(default_channel_header, default_channel_index, enable_channel_scan=True),
+                        specifier=0)
+  change_access_profile(test_device,
+                        create_access_profile(default_channel_header, default_channel_index, enable_channel_scan=True),
+                        specifier=1)
+  set_active_access_class(test_device, 0x01)
+  sleep(0.2)  # give some time to switch AP
+  test_device.clear_unsolicited_responses_received()
+  return test_device
 
 @given("a responder, continuously listening for foreground packets")
 def responder(dut, default_channel_header, default_channel_index):
@@ -46,6 +71,7 @@ def responder(dut, default_channel_header, default_channel_index):
 
 @given("a dormant session registered at the responder for the UID of the requester")
 def dormant_session(test_device, responder, context):
+  context.timeout_ticks = 10000
   interface_config = Configuration(
     qos=QoS(resp_mod=ResponseMode.RESP_MODE_ANY),
     addressee=Addressee(
@@ -53,7 +79,7 @@ def dormant_session(test_device, responder, context):
       id_type=IdType.UID,
       id=int(test_device.uid, 16)
     ),
-    dorm_to=CT.compress(10000)
+    dorm_to=CT.compress(context.timeout_ticks)
   )
 
   context.dormant_request = Command.create_with_return_file_data_action(
@@ -87,6 +113,31 @@ def send_unicast(requester, dut, context):
 
   context.requester_response = requester.execute_command(request, timeout_seconds=10)
 
+@when("the requester starts a broadcast session to the responder")
+def send_unicast(requester, dut, context):
+  interface_config = Configuration(
+    qos=QoS(resp_mod=ResponseMode.RESP_MODE_ALL),
+    addressee=Addressee(
+      access_class=0x11,
+      id_type=IdType.NOID
+    ),
+  )
+
+  request = Command.create_with_return_file_data_action(
+    file_id=0x40,
+    data=[0],
+    interface_type=InterfaceType.D7ASP,
+    interface_configuration=interface_config
+  )
+
+  context.requester_response = requester.execute_command(request, timeout_seconds=10)
+
+
+@when("waiting for the dormant session to time out")
+def waiting_for_dormant_timeout(context, responder):
+  assert len(responder.get_unsolicited_responses_received()) == 0
+  sleep(context.timeout_ticks * 1.2 / 1024) # make sure we sleep long enough
+
 @then("the requester's session should complete successfully")
 def requester_session_should_complete_successfully(context):
   # the tag response command is the last one
@@ -98,11 +149,11 @@ def requester_session_should_complete_successfully(context):
 def responder_should_receive_packet(responder):
   wait_for_unsolicited_response(responder)
 
-  assert len(responder.get_unsolicited_responses_received()) == 1, \
-    "DUT should have received 1 unsolicited response from test device"
+  for resp in responder.get_unsolicited_responses_received():
+    print("resp {}".format(resp))
 
-  responder.clear_unsolicited_responses_received()
-  print "clear_unsolicited_responses_received"
+  assert len(responder.get_unsolicited_responses_received()) >= 1, \
+    "DUT should have received 1 unsolicited response from test device"
 
 
 @then('the requester should receive the dormant session')
@@ -112,12 +163,33 @@ def requester_should_receive_dormant_session(requester):
   assert len(requester.get_unsolicited_responses_received()) == 1, \
     "test should have received 1 unsolicited response from DUT"
 
+
+@then('the requester should not receive the dormant session')
+def requester_should_not_receive_dormant_session(requester):
+  sleep(15)
+
+  assert len(requester.get_unsolicited_responses_received()) == 0, \
+    "requester should have received no dormant session from responder"
+
 @then("the responders's dormant session should complete successfully")
 def dormant_session_should_complete(context, responder):
-  wait_for_unsolicited_response(responder)
+  session_completed = False
+  while not session_completed:
+    for resp in responder.get_unsolicited_responses_received():
+      if resp.tag_id == context.dormant_session_tag_id and resp.execution_completed:
+        assert not resp.completed_with_error, "Execution should be completed without error"
+        session_completed = True
 
-  resp = responder.get_unsolicited_responses_received()
-  assert resp[-1].execution_completed, "Execution not completed"
-  assert not resp[-1].completed_with_error, "Completed with error"
-  assert resp[-1].tag_id == context.dormant_session_tag_id, "Tag ID does not match dormant session tag ID"
+    sleep(0.1)
+
+@then("the responders's dormant session should not complete successfully")
+def dormant_session_should_not_complete(context, responder):
+  session_completed = False
+  while not session_completed:
+    for resp in responder.get_unsolicited_responses_received():
+      if resp.tag_id == context.dormant_session_tag_id and resp.execution_completed:
+        assert resp.completed_with_error, "Execution should be completed with error"
+        session_completed = True
+
+    sleep(0.1)
 
