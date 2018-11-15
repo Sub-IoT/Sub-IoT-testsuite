7fd36f69f9d25e8f11932003d6e6ecf2945e842b
Glenn Ergeerts <glenn.ergeerts@aloxy.io>
2018-11-15 16:48:15 +0100

    pass pytest exit code to shell so gitlab can use this to determine outcome

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
-import argparse
 import sys
 import pytest
 
 sys.path.append('lib/pyd7a')
 args = ["--timeout=200", "--cucumber-json-expand", "--cucumberjson=cucumber.json", "--gherkin-terminal-reporter"]
 args.extend(sys.argv[1:])
-pytest.main(args=args)
+sys.exit(pytest.main(args=args))
