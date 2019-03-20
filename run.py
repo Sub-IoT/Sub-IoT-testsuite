9b2feb8ba2d05679f99884c5ae4b24a7b2b9cb19
Glenn Ergeerts <glenn.ergeerts@aloxy.io>
2019-03-20 08:45:23 +0100

    increase timeouts

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ import sys
 import pytest
 
 sys.path.append('lib/pyd7a')
-args = ["--timeout=120", "--cucumber-json-expand", "--cucumberjson=cucumber.json", "--gherkin-terminal-reporter"]
+args = ["--timeout=200", "--cucumber-json-expand", "--cucumberjson=cucumber.json", "--gherkin-terminal-reporter"]
 args.extend(sys.argv[1:])
 sys.exit(pytest.main(args=args))
